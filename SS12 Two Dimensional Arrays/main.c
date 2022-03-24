3bf62ed7e66a185dab37a98efb9faa222e60260e
mxt629 <xuantien.06021990@gmail.com>
2022-03-24 11:17:56 +0700

    Initial Commit

---
+++ SS12 Two Dimensional Arrays/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SS12 Two Dimensional Arrays
+//
+//  Created by Little Chick on 24/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
